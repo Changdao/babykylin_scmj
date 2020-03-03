d60c0bd96b31e8665722a1e014cdacca6984a144
Changdao <jameslau@263.net>
2020-03-04 06:44:20 +0800

    formalize git and packages

--- server/start_all_mac.sh
+++ server/start_all_mac.sh
@@ -1,3 +1,3 @@
-nohup node ./account_server/app.js ../configs_mac.js &
-nohup node ./hall_server/app.js ../configs_mac.js &
-nohup node ./game_server/app.js ../configs_mac.js &
+nohup node ./account_server/app.js ../configs_mac.js >nohup.account.out &
+nohup node ./hall_server/app.js ../configs_mac.js >nohup.hall.out &
+nohup node ./game_server/app.js ../configs_mac.js >nohup.game.out &
